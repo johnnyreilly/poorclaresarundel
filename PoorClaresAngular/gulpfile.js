cc79acfd74abb48e0f9ede923137e9bcd55ebab1
johnnyreilly <johnny_reilly@hotmail.com>
2015-05-02 18:07:21 +0100

    No longer using MVC - just Web API

--- PoorClaresAngular/gulpfile.js
+++ PoorClaresAngular/gulpfile.js
@@ -1,34 +1,91 @@
 ﻿var gulp = require("gulp");
+
+var concat = require("gulp-concat");
+var ignore = require("gulp-ignore");
+var less = require('gulp-less');
+var sourcemaps = require('gulp-sourcemaps');
+var minifyCss = require("gulp-minify-css");
+var uglify = require("gulp-uglify");
+var rev = require("gulp-rev");
+var path = require("path");
+var eventStream = require("event-stream");
 var gulpUtil = require("gulp-util");
 var del = require("del");
+var wiredep = require("wiredep");
 var yargs = require("yargs").argv;
+var order = require("gulp-order");;//
+var inject = require("gulp-inject");
+
+var config = require("./gulpfile.config.js");
 
 var isDebug = yargs.mode === "Debug";
 gulpUtil.log(gulpUtil.colors.green("isDebug: " + isDebug));
 
+/**
+ * Get the scripts or styles the app requires by combining bower dependencies and app dependencies
+ * 
+ * @param {string} jsOrCss Should be "js" or "css"
+ */
+function getScriptsOrStyles(jsOrCss) {
+
+    var bowerScriptsOrStylesAbsolute = wiredep(config.wiredepOptions)[jsOrCss];
+
+    var bowerScriptsOrStylesRelative = bowerScriptsOrStylesAbsolute.map(function makePathRelativeToCwd(file) {
+        return path.relative('', file);
+    });
+
+    var appScriptsOrStyles = bowerScriptsOrStylesRelative.concat(jsOrCss === "js" ? config.scripts : []);
+
+    return appScriptsOrStyles;
+}
+
+/**
+ * Get the scripts the app requires
+ */
+function getScripts() {
+
+    // Exclude bootstrap.js from the injected scripts
+    var bowerScripts = getScriptsOrStyles("js").filter(function(scriptName) {
+        return scriptName.indexOf("bootstrap.js") === -1;
+    });
+
+    //gulpUtil.log(JSON.stringify(bowerScripts));
+
+    var scripts = bowerScripts.concat(config.scripts);
+
+    return scripts;
+}
+
+/**
+ * Get the scripts the app requires
+ */
+function getScriptsStream(isDebug) {
+
+    var options = isDebug ? { base: config.base } : undefined;
+    var scriptsStream = gulp.src(getScripts(), options);
+
+    return scriptsStream;
+}
+
+/**
+ * Get the styles the app requires
+ */
+function getStyles() {
+
+    return getScriptsOrStyles("css");
+}
+
+
 gulp.task("clean", function (cb) {
 
-    gulpUtil.log("Delete installed bower assets");
+    gulpUtil.log("Delete installed bower assets and build folder");
 
     return del([
+        config.buildDir,
         "./content/fonts",
-        "./content/less",
-        "./scripts/*.js/"
+        "./content/less"
     ], { force: false },
     cb);
-});
-
-gulp.task("install-bower-js-dependencies-into-project", ["clean"], function () {
-
-    return gulp
-        .src([
-            "./bower_components/jquery/dist/jquery.js",
-            "./bower_components/angular/angular.js",
-            "./bower_components/angular-animate/angular-animate.js",
-            "./bower_components/angular-route/angular-route.js",
-            "./bower_components/angular-bootstrap/ui-bootstrap-tpls.js"
-        ])
-        .pipe(gulp.dest("./scripts"));
 });
 
 gulp.task("install-bower-css-dependencies-into-project", ["clean"], function () {
@@ -41,8 +98,90 @@ gulp.task("install-bower-css-dependencies-into-project", ["clean"], function () 
         .pipe(gulp.dest("./content/"));
 });
 
+gulp.task("scripts-debug", ["clean"/*, "install-bower-js-dependencies-into-project" */,"install-bower-css-dependencies-into-project"], function () {
+
+    gulpUtil.log("Copy across all JavaScript files to build/debug");
+
+    return getScriptsStream(true)
+        .pipe(gulp.dest(config.debugFolder));
+});
+
+gulp.task("scripts-release", ["clean"/*, "install-bower-js-dependencies-into-project"*/, "install-bower-css-dependencies-into-project"], function () {
+
+    gulpUtil.log("Concatenate & Minify JS for release into a single file");
+
+    return getScriptsStream(false)
+        .pipe(ignore.exclude("**/*.{ts,js.map}")) // Exclude ts and js.map files - not needed in release mode
+        .pipe(concat("app.js"))                   // Make a single file                                                         
+        .pipe(uglify())                           // Make the file titchy tiny small
+        .pipe(rev())                              // Suffix a version number to it
+        .pipe(gulp.dest(config.releaseFolder));   // Write single versioned file to build/release folder
+});
+
+gulp.task("styles-debug", ["clean"/*, "install-bower-js-dependencies-into-project"*/, "install-bower-css-dependencies-into-project"], function () {
+
+    gulpUtil.log("Copy across all CSS files to build/debug");
+
+    var bowerCss = gulp.src(getStyles(), { base: config.base });
+
+    var appCss = gulp.src(config.styles)
+        .pipe(sourcemaps.init())
+        .pipe(less())
+        .pipe(sourcemaps.write());
+
+    return eventStream.merge(bowerCss, appCss)
+        .pipe(gulp.dest(config.debugFolder));
+});
+
+gulp.task("styles-release", ["clean"/*, "install-bower-js-dependencies-into-project"*/, "install-bower-css-dependencies-into-project"], function () {
+
+    gulpUtil.log("Copy across all files in config.styles to build/debug");
+
+    var bowerCss = gulp.src(getStyles());
+    var appCss = gulp.src(config.styles).pipe(less());
+
+    return eventStream.merge(bowerCss, appCss)
+        .pipe(concat("app.css"))                                   // Make a single file
+        .pipe(minifyCss())                                         // Make the file titchy tiny small
+        .pipe(rev())                                               // Suffix a version number to it
+        .pipe(gulp.dest(config.releaseFolder + "/" + config.css)); // Write single versioned file to build/release folder
+});
+
+gulp.task("inject-debug", ["styles-debug", "scripts-debug"], function () {
+
+    gulpUtil.log("Inject debug links and script tags into " + config.bootFile);
+
+    var scriptsAndStyles = [].concat(getScripts(), getStyles());
+
+    return gulp
+        .src(config.bootFile)
+        .pipe(inject(
+                gulp.src([config.debugFolder + "**/*.{js,css}"], { read: false })
+                    .pipe(order(scriptsAndStyles))
+            ))
+        .pipe(gulp.dest(config.buildDir));
+});
+
+gulp.task("inject-release", ["styles-release", "scripts-release"], function () {
+
+    gulpUtil.log("Inject release links and script tags into " + config.bootFile);
+
+    return gulp
+        .src(config.bootFile)
+        .pipe(inject(gulp.src(config.releaseFolder + "**/*.{js,css}", { read: false }), { removeTags: true }))
+        .pipe(gulp.dest(config.buildDir));
+});
+
+gulp.task("build",
+    isDebug 
+        ? ["inject-debug"]
+        : ["inject-release"],
+    function () {
+
+    });
+
 gulp.task("default",
-    ["install-bower-js-dependencies-into-project", "install-bower-css-dependencies-into-project"],
+    ["build"],
     function () {
         // place code for your default task here
-});
+    });
