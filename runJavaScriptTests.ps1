b21877adddc27b859905b4a675a4256b7d7065de
John Reilly <johnny_reilly@hotmail.com>
2016-01-02 19:05:50 +0000

    remove unit tests project and fix chutzpah lookup

--- runJavaScriptTests.ps1
+++ runJavaScriptTests.ps1
@@ -1,5 +1,5 @@
 # Locate Chutzpah
-$ChutzpahDir = get-childitem chutzpah.console.exe -recurse | select-object -first 1 | select -expand Directory
+$ChutzpahDir = get-childitem -Path packages -Filter chutzpah.console.exe -recurse | select-object -first 1 | select -expand Directory
 
 # Run tests using Chutzpah and export results as JUnit format to chutzpah-results.xml
 $ChutzpahCmd = "$($ChutzpahDir)\chutzpah.console.exe $($env:APPVEYOR_BUILD_FOLDER)\PoorClaresAngular /junit .\chutzpah-results.xml"
