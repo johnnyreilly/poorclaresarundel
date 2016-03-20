8bf4817b65891b18f006df190a945a598878f895
John Reilly <johnny_reilly@hotmail.com>
2016-03-20 15:39:52 +0000

    switch to run tests with karma

--- runJavaScriptTests.ps1
+++
@@ -1,8 +0,0 @@
-# Locate Chutzpah
-$ChutzpahDir = get-childitem -Path packages -Filter chutzpah.console.exe -recurse | select-object -first 1 | select -expand Directory
-
-# Run tests using Chutzpah and export results as JUnit format to chutzpah-results.xml
-$ChutzpahCmd = "$($ChutzpahDir)\chutzpah.console.exe $($env:APPVEYOR_BUILD_FOLDER)\PoorClaresAngular /junit .\chutzpah-results.xml"
-
-Write-Host $ChutzpahCmd
-Invoke-Expression $ChutzpahCmd
