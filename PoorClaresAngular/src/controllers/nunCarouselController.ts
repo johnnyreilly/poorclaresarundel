81fa52c0a40d201ddd0c90440e8022abef17d43e
John Reilly <johnny_reilly@hotmail.com>
2016-05-04 22:06:20 +0100

    upgraded to angular 1.5 started using prefixes for bootstrap

--- PoorClaresAngular/src/controllers/nunCarouselController.ts
+++ PoorClaresAngular/src/controllers/nunCarouselController.ts
@@ -1,39 +1,33 @@
-﻿interface Slide { image: string; text: string; }
-
-interface CarouselScope extends ng.IScope {
-    myInterval: number;
-    addSlide: Function;
-    slides: Slide[];
-}
+﻿interface Slide { image: string; text: string; id: number }
 
 export const nunCarouselControllerName = "NunCarouselController";
 export class NunCarouselController {
+    myInterval: number = 3000;
 
-    static $inject = ["$scope"];
-    constructor(
-        private $scope: CarouselScope) {
-
-        $scope.myInterval = 3000;
-        const slides: Slide[] = $scope.slides = [
-            {
-                image: "/images/main/Page2CommunityAtPrayer.JPG",
-                text: "Called to a life of prayer"
-            }, {
-                image: "/images/main/Page3SusannaAtPrayer.JPG",
-                text: "we seek to live the Gospel"
-            }, {
-                image: "/images/main/Page4ChapelGroupAtPrayer.JPG",
-                text: "in and for"
-            }, {
-                image: "/images/main/Page5GroupOnLawn.JPG",
-                text: "our world of today."
-            }, {
-                image: "/images/main/Page6YohaanaInLibrary.JPG",
-                text: "We share our lives"
-            }, {
-                image: "/images/main/Page7ProvidenceGroup.jpg",
-                text: "and all that we do."
-            }];
-    }
-
+    slides: Slide[] = [
+        {
+            id: 0,
+            image: "/images/main/Page2CommunityAtPrayer.JPG",
+            text: "Called to a life of prayer"
+        }, {
+            id: 1,
+            image: "/images/main/Page3SusannaAtPrayer.JPG",
+            text: "we seek to live the Gospel"
+        }, {
+            id: 2,
+            image: "/images/main/Page4ChapelGroupAtPrayer.JPG",
+            text: "in and for"
+        }, {
+            id: 3,
+            image: "/images/main/Page5GroupOnLawn.JPG",
+            text: "our world of today."
+        }, {
+            id: 4,
+            image: "/images/main/Page6YohaanaInLibrary.JPG",
+            text: "We share our lives"
+        }, {
+            id: 5,
+            image: "/images/main/Page7ProvidenceGroup.jpg",
+            text: "and all that we do."
+        }];
 }
