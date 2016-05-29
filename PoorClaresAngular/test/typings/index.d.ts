256bc835124adf7ff73322e407e078184e3598cd
John Reilly <johnny_reilly@hotmail.com>
2016-05-29 09:20:11 +0100

    Migrate to separate typings.jsons

--- PoorClaresAngular/test/typings/index.d.ts
+++ PoorClaresAngular/test/typings/index.d.ts
@@ -2,5 +2,6 @@
 /// <reference path="globals/angular-mocks/index.d.ts" />
 /// <reference path="globals/angular-ui-router/index.d.ts" />
 /// <reference path="globals/angular/index.d.ts" />
+/// <reference path="globals/define-plugin-variables/index.d.ts" />
 /// <reference path="globals/jasmine/index.d.ts" />
 /// <reference path="globals/jquery/index.d.ts" />
